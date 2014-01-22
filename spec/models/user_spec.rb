66eb090311d6aad59d39b0862609cceac29340c7
John Davison <johncdavison@gmail.com>
2014-01-22 14:18:38 -0800

    Test suite with guard functioning.
    -  Adding basic coverage.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "requires valid name" do
+    user = build :user, name: nil
+    user.valid?.should be_false
+  end
+
+  it "requires valid email" do
+    user = build :user, email: nil
+    user.valid?.should be_false
+  end
+
+  it "requires valid password" do
+    user = build :user, password: nil
+    user.valid?.should be_false
+  end
 end
