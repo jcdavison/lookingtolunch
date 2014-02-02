f674f9634fb4aeee53dbda2592972964c2fdcb12
John Davison <johncdavison@gmail.com>
2014-02-02 14:33:48 -0800

    Debug deploy to heroku.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 if Rails.env == "test"
   $redis = Redis::Namespace.new("lookingtolunchtest", :redis => Redis.new)
 elsif Rails.env == "production"
-  uri = URI.parse ENV['REDISTOGO_URL']
+  uri = URI.parse(ENV['REDISTOGO_URL'])
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
   $redis = Redis::Namespace.new("lookingtolunch", :redis => Redis.new)
